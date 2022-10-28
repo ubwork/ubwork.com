430a625acd15e256c97e9be4dd7c4639e4210f83
nguyenngocky <nguyenngocky1995@gmail.com>
2022-10-28 22:08:57 +0700

    up

--- resources/views/admin/customer/add.blade.php
+++ resources/views/admin/customer/add.blade.php
@@ -39,18 +39,60 @@
                       <div class="col-sm-6">
                         <div class="form-group">
                           <label>password</label>
-                          <input type="password" class="form-control" name="email">
+                          <input type="password" class="form-control" name="password">
                         </div>
                     </div>
                     </div>
                     <div class="row">
-                        <div class="form-group">
-                            <label class="form-label w-100">Ảnh</label>
-                            <img id="image" src="https://png.pngtree.com/element_our/png/20181206/users-vector-icon-png_260862.jpg" alt="your image"
-                                style="max-width: 200px; height:100px; margin-bottom: 10px;" class="img-fluid"/>
-                            <input name="image" type="file" id="img">
-                            <small class="form-text text-muted">Chọn ảnh kích thước nhỏ hơn 5mb</small>
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                              <label class="form-label w-100">Ảnh</label>
+                              <img id="image" src="https://png.pngtree.com/element_our/png/20181206/users-vector-icon-png_260862.jpg" alt="your image"
+                                  style="max-width: 200px; height:100px; margin-bottom: 10px;" class="img-fluid"/>
+                              <input name="image" type="file" id="img">
+                              <small class="form-text text-muted">Chọn ảnh kích thước nhỏ hơn 5mb</small>
+                            </div>
                         </div>
+                        <div class="col-sm-6">
+                          <div class="form-group">
+                            <label>Địa chỉ</label>
+                            <input type="text" class="form-control" name="address">
+                          </div>
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col-sm-6">
+                        <label class="form-label w-100">Giới tính</label>
+                        <div class="d-flex">
+                          <div class="form-check mr-3">
+                            <input type="radio" class="form-check-input" id="radio1" name="gender" value="1" checked>Nam
+                            <label class="form-check-label" for="radio1"></label>
+                          </div>
+                          <div class="form-check mr-3">
+                            <input type="radio" class="form-check-input" id="radio2" name="gender" value="2">Nữ
+                            <label class="form-check-label" for="radio2"></label>
+                          </div>
+                          <div class="form-check">
+                            <input type="radio" class="form-check-input" id="radio3" name="gender" value="3">Khác
+                            <label class="form-check-label" for="radio3"></label>
+                          </div>
+                        </div>
+                      </div>
+
+                      
+                      <div class="col-sm-6">
+                        <label class="form-label w-100">Trạng thái</label>
+                        <div class="d-flex">
+                          <div class="form-check mr-3">
+                            <input type="radio" class="form-check-input" id="status" name="status" value="1" checked>Hoạt động
+                            <label class="form-check-label" for="status"></label>
+                          </div>
+                          <div class="form-check">
+                            <input type="radio" class="form-check-input" id="status2" name="status" value="0">Không hoạt động
+                            <label class="form-check-label" for="status2"></label>
+                          </div>
+                        </div>
+                      </div>
                     </div>
                     <div class="mt-3">
                         <input type="submit" value="Thêm" class="btn btn-success float-left mr-3">
