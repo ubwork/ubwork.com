e79ae94d8e6e763c71619bd8357cadea448184df
LgLinh <luonglinh120@gmail.com>
2022-10-29 23:13:21 +0700

    up

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,12 +3,45 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\AuthRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Redirect;
+use App\Models\Customers;
 
 class LoginController extends Controller
 {
     //
-    public function getLogin(){
+    private $v;
+    public function __construct()
+    {
+        $this->v = [];
+    }
+    // 
+    public function getLogin()
+    {
         return view('login.login');
+    }
+
+    public function postLogin(AuthRequest $request)
+    {
+        $email = $request->input('email');
+        // $password = password_hash($request->input('password'), PASSWORD_ARGON2I);
+
+        // password_verify('adsadsasasa',$hash);
+        dd(Auth::user());
+        $password = $request->input('password');
+        if (Auth::attempt(['email' => $email, 'password' => $password])) {
+            return Redirect::to('/');
+        } else {
+            Session::flash('error', 'Email hoặc mật khẩu không đúng');
+            return Redirect::to('/login');
+        }
+    }
+
+    public function getLogOut()
+    {
+        Auth::logout();
+        return Redirect::to('/login');
     }
 }
