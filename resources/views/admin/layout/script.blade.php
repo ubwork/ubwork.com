95f07e9a8ae7589f5f8a884b0855da71a7cfefc0
LgLinh <luonglinh120@gmail.com>
2022-11-01 21:53:47 +0700

    update

--- resources/views/admin/layout/script.blade.php
+++ resources/views/admin/layout/script.blade.php
@@ -1,5 +1,4 @@
   <!-- jQuery -->
-  @include('sweetalert::alert')
   
   <script src="{{ asset('assets/admin-bower/plugins/jquery/jquery.min.js') }}"></script>
   <!-- jQuery UI 1.11.4 -->
@@ -15,5 +14,7 @@
   <!-- AdminLTE App -->
   <script src="{{ asset('assets/admin-bower/dist/js/adminlte.js') }}"></script>
   <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+  <script src="{{ asset('assets/admin-bower/plugins/sweetalert2/sweetalert2.all.min.js') }}"></script>
+  <script src="{{ asset('assets/admin-bower/plugins/toastr/toastr.min.js') }}"></script>
   <script src="{{ asset('js/app.js') }}"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
