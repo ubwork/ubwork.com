dcf48d2d84e591bd3697a533fe15b568de8e8b42
TmT <108978840+zoudey@users.noreply.github.com>
2022-11-02 03:53:55 +0700

    list

---
+++ app/Models/Candidate.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Candidate extends Model
+{
+    use HasFactory;
+    protected $table = 'candidates';
+    protected $fillable = [
+        'name',
+        'avatar',
+        'email',
+        'password',
+        'phone',
+        'address',
+        'position',
+        'gender',
+        'city',
+        'coin',
+        'status'
+    ];
+}
