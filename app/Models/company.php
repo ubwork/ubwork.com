ee8366cdfdcf72d15f3120444a1c55ff9f12049e
nguyenngocky <nguyenngocky1995@gmail.com>
2022-11-08 20:21:17 +0700

    compay lỗi t bắt buộc phải đẩy

--- app/Models/company.php
+++ app/Models/company.php
@@ -7,8 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
-
-class Company extends Model
+use Illuminate\Foundation\Auth\User as Authenticatable;
+class Company  extends Authenticatable
 {
     use HasFactory;
     protected $table = "companies";
