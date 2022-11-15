930ea77100149bcfe1d3efe2f2f022cebe02dd6c
LgLinh <luonglinh120@gmail.com>
2022-11-16 00:17:54 +0700

    Revert "up"
    This reverts commit 4467695bcf2b2fa0c6201a4c6e7eca614895f23d.

--- resources/views/client/company/company.blade.php
+++ resources/views/client/company/company.blade.php
@@ -122,6 +122,7 @@
                             {{-- <p>Showing 36 of 497 Jobs</p>
                             <div class="bar"><span class="bar-inner" style="width: 40%"></span></div>
                             <button class="show-more">Xem thêm</button> --}}
+                            {{$data->links()}}
                         </div>
                     </div>
                 </div>
