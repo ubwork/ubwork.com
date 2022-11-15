81d2c35acdba157bee3c90d4fe1309d9cb909043
TmT <108978840+zoudey@users.noreply.github.com>
2022-11-15 15:43:32 +0700

    fix upcv

--- app/Http/Controllers/Client/SeekerController.php
+++ app/Http/Controllers/Client/SeekerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\client;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\client\Upcv;
 use App\Models\Major;
 use App\Models\SeekerProfile;
 use Illuminate\Http\Request;
@@ -15,38 +16,62 @@ class SeekerController extends Controller
         $data = SeekerProfile::where('candidate_id', auth('candidate')->user()->id)->paginate(2);
         // dd($data);
         $maJor = Major::all();
-        return view('client.upcv.upcv', compact('data','maJor'));
+        return view('client.upcv.upcv', compact('data', 'maJor'));
     }
-    public function store(Request $request)
+    public function store(Upcv $request)
     {
+        $request->validate([
+            'path_cv' => 'required'
+        ]);
         $seeker = new SeekerProfile;
-        $seeker->candidate_id = auth('candidate')->user()->id;
-        $seeker->name = $request->name;
-        $seeker->position_candidate = $request->position_candidate;
-        $seeker->major_id = $request->major_id;
-        $seeker->description = $request->description;
-        $get_pdf = $request->file('path_cv');
-        $path_pdf = 'upload/cv';
-        if ($get_pdf) {
-            $get_name_pdf = $get_pdf->getClientOriginalName();
-            $name_pdf = current(explode('.', $get_name_pdf));
-            $new_pdf = $name_pdf . rand(0, 99) . '.' . $get_pdf->getClientOriginalExtension();
-            $get_pdf->move($path_pdf, $new_pdf);
-            $seeker->path_cv = $new_pdf;
+        $data = SeekerProfile::where('candidate_id', auth('candidate')->user()->id)->first();
+        // dd($data);
+        if (!empty($data)) {
+            $get_pdf = $request->file('path_cv');
+            $path_pdf = 'upload/cv';
+            if ($request->hasFile('path_cv')) {
+                $get_name_pdf = $get_pdf->getClientOriginalName();
+                $name_pdf = current(explode('.', $get_name_pdf));
+                $new_pdf = $name_pdf . rand(0, 99) . '.' . $get_pdf->getClientOriginalExtension();
+                $get_pdf->move($path_pdf, $new_pdf);
+            }
+            $file_path = public_path('upload/cv/' . $data->path_cv);
+            if (is_file($file_path)) {
+                unlink($file_path);
+            }
+            $data->update([
+                'path_cv' => $new_pdf,
+            ]);
+        } else {
+            $seeker->candidate_id = auth('candidate')->user()->id;
+            $seeker->name = $request->name;
+            $seeker->position_candidate = $request->position_candidate;
+            $seeker->major_id = $request->major_id;
+            $seeker->description = $request->description;
+            $get_pdf = $request->file('path_cv');
+            $path_pdf = 'upload/cv';
+            if ($get_pdf) {
+                $get_name_pdf = $get_pdf->getClientOriginalName();
+                $name_pdf = current(explode('.', $get_name_pdf));
+                $new_pdf = $name_pdf . rand(0, 99) . '.' . $get_pdf->getClientOriginalExtension();
+                $get_pdf->move($path_pdf, $new_pdf);
+                $seeker->path_cv = $new_pdf;
+            }
+            $seeker->email = $request->email;
+            $seeker->phone = $request->phone;
+            $seeker->save();
         }
-        $seeker->email = $request->email;
-        $seeker->phone = $request->phone;
-        $seeker->save();
-        return redirect('seeker');
+        return redirect('seeker')->with('success', 'Thêm Thành Công');
     }
     public function destroy($id)
     {
         $seeker = SeekerProfile::find($id);
-        $file_path = public_path('upload/cv/'.$seeker->path_cv);
-        if(is_file($file_path)){
+        $file_path = public_path('upload/cv/' . $seeker->path_cv);
+        if (is_file($file_path)) {
             unlink($file_path);
         }
-        $seeker->delete();
+        $seeker->path_cv = "";
+        $seeker->save();
         return redirect('seeker');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\client\PdfController;
 use Illuminate\Support\Facades\Route;
 
 /*
