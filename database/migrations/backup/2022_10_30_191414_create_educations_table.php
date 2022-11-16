8f8eca1a2fce25e4ef837521ac74895c0b8ca106
LgLinh <luonglinh120@gmail.com>
2022-11-16 23:14:07 +0700

    up

---
+++ database/migrations/backup/2022_10_30_191414_create_educations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('educations', function (Blueprint $table) {
+            $table->id();
+            $table->integer('seeker_id');
+            $table->string('type_degree');
+            $table->string('major_id');
+            $table->string('name_education');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->decimal('gpa');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('educations');
+    }
+};
--- database/migrations/backup/2022_11_08_142033_create_feedback_company_table.php
+++ database/migrations/backup/2022_11_08_142033_create_feedback_company_table.php
@@ -13,15 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('feedback', function (Blueprint $table) {
+        Schema::create('feedback_company', function (Blueprint $table) {
             $table->id();
             $table->integer('candidate_id');
             $table->integer('company_id');
             $table->tinyInteger('rate');
             $table->string('comment');
-            $table->string('satisfied')->nullable();
-            $table->string('unsatisfied')->nullable();
-            $table->integer('is_candidate')->comment('0:feedback candidate, 1:feedback company');
+            $table->string('satisfied');
+            $table->string('unsatisfied');
             $table->timestamps();
         });
     }
@@ -33,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('feedback');
+        Schema::dropIfExists('feedback_company');
     }
 };
