8d117971b33aed57806ac590d666e72861be407f
nguyenngocky <nguyenngocky1995@gmail.com>
2022-11-16 22:56:10 +0700

    sửa trang đổi mật khẩu

--- resources/views/client/candidate/change-password.blade.php
+++ resources/views/client/candidate/change-password.blade.php
@@ -18,44 +18,28 @@
                   <form class="default-form" action="{{route("update_pass")}}" method="post" enctype="multipart/form-data">
                     @csrf
                     <div class="row">
+                      <div class="form-group col-lg-6 col-md-12">
+                        <label>Mật khẩu cũ</label>
+                        <input type="password" name="password_old" placeholder="Mật khẩu cũ" value="">
+                        @error('password_old')
+                            <small class="text-danger">{{$message}}</small>
+                        @enderror
+                      </div>
                       <!-- Input -->
                       <div class="form-group col-lg-6 col-md-12">
                         <label>Mật khẩu mới</label>
-                        <input type="text" name="password" placeholder="Mật khẩu mới" value="">
+                        <input type="password" name="password" placeholder="Mật khẩu mới" value="">
                         @error('password')
                             <small class="text-danger">{{$message}}</small>
                         @enderror
                       </div>
                       <div class="form-group col-lg-6 col-md-12">
                         <label>Nhập lại mật khẩu</label>
-                        <input type="text" name="re_password" placeholder="Nhập lại mật khẩu" value="">
-                        @error('password')
+                        <input type="password" name="re_password" placeholder="Nhập lại mật khẩu" value="">
+                        @error('re_password')
                             <small class="text-danger">{{$message}}</small>
                         @enderror
                       </div>
-                        @if ( Session::has('success') )
-                            <div class="alert alert-success alert-outline alert-dismissible" role="alert">
-                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                                <div class="alert-icon">
-                                    <i class="far fa-fw fa-bell"></i>
-                                </div>
-                                <div class="alert-message">
-                                    <strong>{{ Session::get('success') }}</strong>
-                                </div>
-                            </div>
-                        @endif
-                        <?php //Hiển thị thông báo lỗi?>
-                        @if ( Session::has('error') )
-                            <div class="alert alert-danger alert-outline alert-dismissible" role="alert">
-                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                                <div class="alert-icon">
-                                    <i class="far fa-fw fa-bell"></i>
-                                </div>
-                                <div class="alert-message">
-                                    <strong>{{ Session::get('error') }}</strong>
-                                </div>
-                            </div>
-                        @endif
                       <div class="w-100"></div>
                       <!-- Input -->
                       <div class="form-group col-lg-6 col-md-12">
