de10ba9ab303446a3995f936dab90939fadabc56
nguyenngocky <nguyenngocky1995@gmail.com>
2022-11-17 09:25:47 +0700

    xem cv moi chi lam giao dien

---
+++ app/Http/Controllers/Company/ViewCvController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Company;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Candidate;
+use App\Models\Certificate;
+use App\Models\Education;
+use App\Models\Experience;
+use App\Models\Major;
+use App\Models\SeekerProfile;
+use App\Models\Skill;
+use App\Models\SkillSeeker;
+use Illuminate\Support\Facades\Session;
+
+class ViewCvController extends Controller
+{
+    private $v;
+    public function __construct()
+    {
+        $this->v = [];
+    }
+
+    public function viewProfile($id)
+    {
+        $seekerProfile = SeekerProfile::where('id', $id)->first();
+        $candidate = Candidate::where('id', $seekerProfile->candidate_id)->first();
+        $this->v['seekerProfile'] = $seekerProfile;
+        $this->v['skills'] = Skill::all();
+        $this->v['major'] = Major::all();
+        $this->v['maJor'] = Major::all();
+
+        if (!empty($seekerProfile)) {
+            $this->v['experiences'] = Experience::where('seeker_id', $seekerProfile->id)->get();
+            $this->v['educations'] = Education::where('seeker_id', $seekerProfile->id)->get();
+            $this->v['list_skill'] = SkillSeeker::where('seeker_id', $seekerProfile->id)->get();
+            $this->v['certificates'] = Certificate::where('seeker_id', $seekerProfile->id)->get();
+
+            return view('company.view-cv.index', $this->v);
+
+        }else {
+            Session::flash('error', 'Không tìm thấy CV!');
+            return redirect()->route('company.filter');
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -105,6 +105,4 @@ Route::get('create-cv/deleteCertificate/{id}', 'Client\CreateCvController@delete
 
 Route::get('create-cv/getPdf', 'Client\CreateCvController@getPdf')->name('getPdf');
 
-Route::get('view-profile-candidate', 'Client\CreateCvController@viewProfile')->name('viewProfile');
-
 Route::get('change-language/{language}', 'LanguageController@changeLanguage')->name('change-language');
