c092a736d879a87624c0a07dcf468a74d2e55a42
TmT <108978840+zoudey@users.noreply.github.com>
2022-11-19 16:27:05 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\client\MailController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,7 +23,7 @@ Route::get('/logout', ['as' => 'logout', 'uses' => 'Client\Auth\LoginController@
 //candidate
 Route::get('/', 'client\HomeController@index')->name('index');
 Route::get('search', 'client\HomeController@search')->name('search');
-Route::get('send', [MailController::class, 'send']);
+Route::get('send', 'client\MailController@send')->name('send');
 
 Route::get('/job', 'client\JobController@job')->name('job');
 Route::get('/job-cat/{id}', 'client\JobController@job_cat')->name('job-cat');
