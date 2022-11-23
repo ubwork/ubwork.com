d781ca8b15254eb5dc85484c31059bf6d6a1368f
TmT <108978840+zoudey@users.noreply.github.com>
2022-11-23 09:20:43 +0700

    update

--- resources/views/email/job-speed.blade.php
+++ resources/views/email/job-speed.blade.php
@@ -8,6 +8,12 @@
       <div class="login-form default-form">
         <div class="form-inner">
           <h3>Tìm Kiếm Công Việc Nhanh</h3>
+          <h5>Cách sử dụng:</h5>
+          <div class="sec-title text-center">
+            <div class="text">Mỗi ngày chỉ được sử dụng tìm kiếm nhanh một lần, và mỗi lần sử dũng ứng viên sẽ phải trả 30 coin.
+                Nếu ứng viên đã sử dụng tìm kiếm nhanh sẽ được kích hoạt chế độ lọc các job có công việc đang có nhu cầu tìm việc gấp có thời gian đăng nhỏ hơn 5 ngày <a href="{{route('job')}}">tại đây!</a>
+            </div>
+        </div>
             <div class="form-group">
               <button class="theme-btn btn-style-one" type="submit" name="log-in"><a href="{{route('send')}}" style="color: #fff">Tìm Kiếm</a></button>
             </div>
