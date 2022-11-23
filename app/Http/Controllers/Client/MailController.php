60eb7ebd68fa77c7d1fee8f543832c1ec6e138e9
TmT <108978840+zoudey@users.noreply.github.com>
2022-11-23 20:50:30 +0700

    update

--- app/Http/Controllers/Client/MailController.php
+++ app/Http/Controllers/Client/MailController.php
@@ -17,41 +17,47 @@ class MailController extends Controller
 {
     public function send()
     {
-        $subject = auth('candidate')->user()->id;
-        $bitcoin = auth('candidate')->user()->coin;
-        $candidate = Candidate::where('status', 1)->where('id', $subject)->first();
-        $seeker = SeekerProfile::where('candidate_id', $subject)->first();
-        $coin = $bitcoin;
-        $date = date('Y/m/d', time());
-        $jobspeed = JobSpeed::where('seeker_id', $subject)->whereDate('created_at', $date)->first(); // hàm sử lý thời gian
-        if (!empty($seeker)) {
-            $major = $seeker->major_id;
-            $job = JobPost::where('major_id', $major)->get();
-            $jobpost = JobPost::where('major_id', $major)->first();
-            if ($coin - 30 < 0) {
-                return back()->with('error', 'Tài Khoản Của Bạn Không Đủ Số Dư Vui Lòng Nạp Thêm Tiền !');
-            } elseif (!isset($jobpost)) {
-                return back()->with('warning', 'Không Có Job Nào Phù Hợp!');
-            } elseif (!empty($jobspeed)) {
-                return back()->with('error', 'Hôm Nay Bạn Đã Sử Dụng Phương Thức Này Rồi Vui Lòng Quay Lại Vào Ngày Mai !');
-            } else {
-                foreach ($job as $item) {
-                    $email = $item->company->email;
-                    $company_name = $item->company->company_name;
-                    Mail::to($email)->send(new SendMail($subject, $company_name));
-                    $speed = new JobSpeed();
-                    $speed->job_post_id = $item->id;
-                    $speed->seeker_id = $seeker->id;
-                    $speed->status = '1';
-                    $speed->save();
+        if (auth('candidate')->check()) {
+            $subject = auth('candidate')->user()->id;
+            $bitcoin = auth('candidate')->user()->coin;
+            $candidate = Candidate::where('status', 1)->where('id', $subject)->first();
+            $seeker = SeekerProfile::where('candidate_id', $subject)->first();
+            $coin = $bitcoin;
+            $date = date('Y/m/d', time());
+            $jobspeed = JobSpeed::where('seeker_id', $subject)->whereDate('created_at', $date)->first(); // hàm sử lý thời gian
+            if (!empty($seeker)) {
+                $major = $seeker->major_id;
+                $job = JobPost::where('major_id', $major)->get();
+                $jobpost = JobPost::where('major_id', $major)->first();
+                if ($coin - 30 < 0) {
+                    return back()->with('error', 'Tài Khoản Của Bạn Không Đủ Số Dư Vui Lòng Nạp Thêm Tiền !');
+                } elseif (!isset($jobpost)) {
+                    return back()->with('warning', 'Không Có Job Nào Phù Hợp!');
+                } elseif (!empty($jobspeed)) {
+                    return back()->with('error', 'Hôm Nay Bạn Đã Sử Dụng Phương Thức Này Rồi Vui Lòng Quay Lại Vào Ngày Mai !');
+                } elseif (!empty($seeker->major)) {
+                    return back()->with('error', 'Vui lòng điền chuyên ngành bạn muốn tìm kiếm vào cv!');
+                } else {
+                    foreach ($job as $item) {
+                        $email = $item->company->email;
+                        $company_name = $item->company->company_name;
+                        Mail::to($email)->send(new SendMail($subject, $company_name));
+                        $speed = new JobSpeed();
+                        $speed->job_post_id = $item->id;
+                        $speed->seeker_id = $seeker->id;
+                        $speed->status = '1';
+                        $speed->save();
+                    }
+                    $candidate->update([
+                        'coin' => $coin - 30,
+                    ]);
                 }
-                $candidate->update([
-                    'coin' => $coin - 30,
-                ]);
+                return back()->with('success', 'Tìm Kiếm Thành Công');
+            } else {
+                return back()->with('error', 'Bạn chưa tạo cv vui lòng tạo cv để sử dụng tính năng này!');
             }
-            return back()->with('success', 'Tìm Kiếm Thành Công');
         } else {
-            return back()->with('error', 'Bạn chưa tạo cv vui lòng tạo cv để sử dụng tính năng này!');
+            return back()->with('error', 'Vui lòng đăng nhập để sử dụng chức năng!');
         }
     }
     public function jobspeed()
