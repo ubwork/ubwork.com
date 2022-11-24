f330db68ab907a520fec753fca76053de4277391
LgLinh <luonglinh120@gmail.com>
2022-11-24 21:21:55 +0700

    up

--- app/Http/Controllers/Company/DetailCandidateController.php
+++ app/Http/Controllers/Company/DetailCandidateController.php
@@ -14,13 +14,22 @@ class DetailCandidateController extends Controller
 {
     public function index(Request $request, $id)
     {
-        $data  =  SeekerProfile::with('major', 'skill', 'candidate')->where('candidate_id', $id)->first();
+        $data  =  SeekerProfile::where('candidate_id', $id)->first();
+        $can = Candidate::where('id',$data->candidate_id)->first();
+
         $title = "Thông tin ứng viên";
         $activeRoute = "Profile";
         $maJor  = Major::all();
         $education = Education::where('seeker_id', $data->id)->get()->toArray();
 
         $exp = Experience::where('seeker_id', $data->id)->get()->toArray();
-        return view('company.detail-candidate.index', compact('title', 'activeRoute', 'maJor', 'data', 'education', 'exp'));
+        return view('company.detail-candidate.index', compact('title', 'activeRoute', 'maJor', 'data', 'education', 'exp', 'can'));
+    }
+    public function feedback($id)
+    {
+        // $company_detail = company::where('id', $id)->first();
+        // $company_job = JobPost::where('company_id', $company_detail->id)->get();
+        // $maJor = Major::all();
+        return view('company.detail-candidate.feedback', compact('maJor'));
     }
 }
