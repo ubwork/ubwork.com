83e46d1aa408e549257aff021fae83c628ef1e31
TmT <108978840+zoudey@users.noreply.github.com>
2022-11-29 01:35:42 +0700

    up

--- resources/views/email/job-speed.blade.php
+++ resources/views/email/job-speed.blade.php
@@ -3,28 +3,33 @@
     {{ __('UB Work') }}
 @endsection
 @section('content')
-    <section class="page-title style-three">
+    {{-- @dd($seeker) --}}
+    <section class="page-title style-three" style="margin-top:100px ">
         <h1>Tìm Kiếm Nhanh</h1>
         <form action="send" method="GET">
             <div class="auto-container">
-                <div class="top-filters" style="margin-top: 100px ">
-                    <div class="form-group">
-                        <select class="chosen-select" name="major">
-                            <option value="">Mời Chọn Chuyên Ngành</option>
-                            @foreach ($maJor as $item)
-                                <option value="{{ $item->id }}">{{ $item->name }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="form-group" style="width:181px">
-                        <select class="chosen-select" name="skill">
-                            <option value="">Mời Chọn Kỹ Năng</option>
-                            @foreach ($skill as $item)
-                                <option value="{{ $item->id }}">{{ $item->name }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                </div>
+                @if (auth('candidate')->check())
+                    @if ($major == null || $skills == null)
+                        <div class="top-filters" style="margin-top: 100px ">
+                            <div class="form-group">
+                                <select class="chosen-select" name="major">
+                                    <option value="">Mời Chọn Chuyên Ngành</option>
+                                    @foreach ($maJor as $item)
+                                        <option value="{{ $item->id }}">{{ $item->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group" style="width:181px">
+                                <select class="chosen-select" name="skill">
+                                    <option value="">Mời Chọn Kỹ Năng</option>
+                                    @foreach ($skill as $item)
+                                        <option value="{{ $item->id }}">{{ $item->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                    @endif
+                @endif
                 <button class="theme-btn btn-style-one" style="margin-top: 50px " type="submit">Tìm Kiếm</button>
             </div>
         </form>
