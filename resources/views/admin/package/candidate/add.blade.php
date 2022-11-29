cdc949e842a1ba7594efd9bad8604f2c370acf2e
LgLinh <luonglinh120@gmail.com>
2022-11-29 10:13:35 +0700

    update

---
+++ resources/views/admin/package/candidate/add.blade.php
@@ -0,0 +1,90 @@
+@extends('admin.layout.app')
+@section('title')
+    {{ __('Package - add') }}
+@endsection
+@section('content')
+<section class="content">
+    <div class="container-fluid">
+        <div class="">
+            <div class="card card-primary">
+                <div class="card-header">
+                  <h3 class="card-title">{{__($title)}}</h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                  <form action="{{ route('admin.package.candidate.store')}}" method="post" enctype="multipart/form-data">
+                    @csrf
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <!-- text input -->
+                            <div class="form-group">
+                                <label for="inputName">{{__('Tên gói nạp')}} <span class="text-danger">*</span></label>
+                                <input type="text" id="inputName" name="title" class="form-control" value="{{old('title')}}">
+                                @error('title')
+                                <small class="text-danger">{{$message}}</small>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <label>{{__('Số coin')}} <span class="text-danger">*</span></label>
+                                <input type="number" class="form-control" name="coin" value="{{old('coin')}}">
+                                @error('coin')
+                                <small class="text-danger">{{$message}}</small>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                    {{--  --}}
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <label>{{__('Giá tiền')}} <span class="text-danger">*</span></label>
+                                <input type="number" class="form-control" name="amount" value="{{old('amount')}}">
+                                @error('amount')
+                                <small class="text-danger">{{$message}}</small>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <label>{{__('Hạn sử dụng (trong bao nhiêu tháng)')}} <span class="text-danger">*</span></label>
+                                <input type="number" class="form-control" name="expired" value="{{old('expired')}}">
+                                @error('expired')
+                                <small class="text-danger">{{$message}}</small>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <label class="form-label w-100">{{__('STATUS')}}</label>
+                            <div class="d-flex">
+                              <div class="form-check mr-3">
+                                <input type="radio" class="form-check-input" id="status" name="status" value="0" checked>{{__('Chưa kích hoạt')}}
+                                <label class="form-check-label" for="status"></label>
+                              </div>
+                              <div class="form-check mr-3">
+                                <input type="radio" class="form-check-input" id="status" name="status" value="1" checked>{{__('kích hoạt')}}
+                                <label class="form-check-label" for="status"></label>
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="mt-3">
+                        <button type="submit" class="btn btn-primary float-left mr-3">{{__('Lưu')}}</button>
+                        <a href="{{route('admin.package.candidate.index')}}" class="btn btn-secondary">{{__('Hủy')}}</a>
+                    </div>
+                  </form>
+                </div>
+                <!-- /.card-body -->
+              </div>
+        </div>
+    </div>
+</section>
+@endsection
+@section('script')
+@parent
+<script src="{{asset('js/admin/candidate.js')}}"></script>
+@endsection
